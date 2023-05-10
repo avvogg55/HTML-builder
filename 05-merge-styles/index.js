7605782acd38039fd734b831277ff917db43d8a6
Gleb Goncharov <avvogg55@gmail.com>
2023-05-10 12:52:02 +0300

    fix: deleted Sync methods

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,13 +5,13 @@ const stylesDir = path.join(__dirname, 'styles');
 const distDir = path.join(__dirname, 'project-dist');
 const bundleFile = 'bundle.css';
 
-const stylesFiles = fs.readdirSync(stylesDir);
+const stylesFiles = fs.readdir(stylesDir);
 
 const cssFiles = stylesFiles.filter((fileName) => path.extname(fileName) === '.css');
 
 const stylesData = cssFiles.map((cssFile) => {
   const cssFilePath = path.join(stylesDir, cssFile);
-  return fs.readFileSync(cssFilePath, 'utf8');
+  return fs.readFile(cssFilePath, 'utf8');
 });
 
-fs.writeFileSync(path.join(distDir, bundleFile), stylesData.join('\n'), 'utf8');
+fs.writeFile(path.join(distDir, bundleFile), stylesData.join('\n'), 'utf8');
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,19 +6,19 @@ const stylesDir = path.join(__dirname, 'styles');
 const assetsDir = path.join(__dirname, 'assets');
 const distDir = path.join(__dirname, 'project-dist');
 
-if (!fs.existsSync(distDir)) {
-  fs.mkdirSync(distDir);
+if (!fs.exists(distDir)) {
+  fs.mkdir(distDir);
 }
 
 const templatePath = path.join(__dirname, 'template.html');
-const template = fs.readFileSync(templatePath, 'utf-8');
+const template = fs.readFile(templatePath, 'utf-8');
 
 const componentNames = template.match(/{{\s*(\S+)\s*}}/g).map(tag => tag.slice(2, -2));
 
 const components = {};
 for (const componentName of componentNames) {
   const componentPath = path.join(componentsDir, `${componentName}.html`);
-  components[componentName] = fs.readFileSync(componentPath, 'utf-8');
+  components[componentName] = fs.readFile(componentPath, 'utf-8');
 }
 
 let result = template;
@@ -28,34 +28,34 @@ for (const [componentName, componentContent] of Object.entries(components)) {
 }
 
 const indexPath = path.join(distDir, 'index.html');
-fs.writeFileSync(indexPath, result);
+fs.writeFile(indexPath, result);
 
 const styles = [];
-const styleFiles = fs.readdirSync(stylesDir);
+const styleFiles = fs.readdir(stylesDir);
 for (const fileName of styleFiles) {
   const filePath = path.join(stylesDir, fileName);
   const ext = path.extname(filePath);
   if (ext === '.css') {
-    const content = fs.readFileSync(filePath, 'utf-8');
+    const content = fs.readFile(filePath, 'utf-8');
     styles.push(content);
   }
 }
 
 const stylesPath = path.join(distDir, 'style.css');
-fs.writeFileSync(stylesPath, styles.join('\n'));
+fs.writeFile(stylesPath, styles.join('\n'));
 
 const assetsDest = path.join(distDir, 'assets');
-if (!fs.existsSync(assetsDest)) {
-  fs.mkdirSync(assetsDest);
+if (!fs.exists(assetsDest)) {
+  fs.mkdir(assetsDest);
 }
 
-const assetsFiles = fs.readdirSync(assetsDir);
+const assetsFiles = fs.readdir(assetsDir);
 for (const fileName of assetsFiles) {
   const srcPath = path.join(assetsDir, fileName);
   const destPath = path.join(assetsDest, fileName);
-  const stat = fs.statSync(srcPath);
+  const stat = fs.stat(srcPath);
   if (stat.isFile()) {
-    const content = fs.readFileSync(srcPath);
-    fs.writeFileSync(destPath, content);
+    const content = fs.readFile(srcPath);
+    fs.writeFile(destPath, content);
   }
 }
